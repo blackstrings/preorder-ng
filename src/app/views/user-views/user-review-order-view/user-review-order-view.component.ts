aa4479135ecf823712753f3bf988cf6b812fe982
Sony Lao <sony@Sonys-iMac.local>
2020-09-01 20:03:18 -0500

    added modal on cartItemList to increment/decrement

--- src/app/views/user-views/user-review-order-view/user-review-order-view.component.ts
+++ src/app/views/user-views/user-review-order-view/user-review-order-view.component.ts
@@ -1,3 +1,11 @@
+import { YesNoModalViewComponent } from './../../common-views/modals/yes-no-modal-view/yes-no-modal-view.component';
+import { NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
+import { AddToOrderValidatorContainer } from './../../../validators/add-to-order-validator-container';
+import { ModalConfig } from './../../common-views/modals/modal-config';
+import { ProductAddModalViewComponent } from './../../common-views/modals/product-add-view/product-add-modal-view.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Product } from 'src/app/models/product/product';
+import { ProductService } from './../../../services/product-service/product.service';
 import {Component, OnInit} from '@angular/core';
 import {CartService} from "../../../services/cart-service/cart.service";
 import {Merchant} from "../../../models/merchant/merchant";
@@ -8,6 +16,7 @@ import {OrderValidator} from "../../../validators/order-validator/order-validato
 import {ViewRoutes} from "../../view-routes";
 import {Router} from "@angular/router";
 import {PaymentService} from "../../../services/payment-service/payment.service";
+import {take} from "rxjs/operators";
 
 /** this view allows user to review their orders or items before going to checkout */
 @Component({
@@ -32,7 +41,8 @@ export class UserReviewOrderViewComponent implements OnInit {
   public checkoutError: boolean = false;
 
   constructor(private cartService: CartService, private userService: UserService, private router: Router,
-              private paymentService: PaymentService) { }
+              private paymentService: PaymentService, private productService: ProductService,
+              private modalService: NgbModal) { }
 
   ngOnInit(): void {
     this.setupViewData();
@@ -108,6 +118,69 @@ export class UserReviewOrderViewComponent implements OnInit {
     this.subTotal = this.order.getTotalPrice();
     this.taxAmount = this.subTotal * 0.055; // hard coded tax
     this.orderTotal = this.subTotal + this.taxAmount;
+  }
+
+  public showModalProductView(id: number): void {
+    if(id) {
+      const product: Product = this.productService.getProductFromCache(id);
+      if(product) {
+
+        // set config to not allow keyboard esc or click on backdrop to close
+        const modalRef = this.modalService.open(ProductAddModalViewComponent, ModalConfig.requiresClickAndScrollable());
+        if(modalRef.componentInstance instanceof ProductAddModalViewComponent) {
+          modalRef.componentInstance.init(product);
+          modalRef.componentInstance.onClose
+            .pipe(take(1))
+            .subscribe( (val: Product) => {
+              if(val) {
+                this.addToOrder(val);
+              }
+            });
+        }
+
+      } else {
+        console.error('<< UserCreateOrderView >> showModalProductView failed, product null');
+      }
+    }
+  }
+
+  private addToOrder(product): void {
+    if(this.merchant && product) {
+      const container: AddToOrderValidatorContainer = new AddToOrderValidatorContainer(product, this.merchant);
+      // if the product is of a different merchant,
+      // we need to let the user know their previous order will have to be discarded
+      if(this.cartService.addToOrderValidate(container)) {
+        this.cartService.addToOrder(container);
+      } else {
+        console.warn('<< UserCreateOrderView >> addToCart interrupted, attempting to add new products from new store');
+        this.showModalOrderOverrideWarning(container);
+      }
+    } else {
+      console.error('<< UserCreateOrderView >> addToCart failed, product null or merchant was not set properly');
+    }
+  }
+
+  /** show warning confirmation if user wants to start a new order overwriting current order */
+  private showModalOrderOverrideWarning(container: AddToOrderValidatorContainer): void {
+    // set modal config to not allow keyboard esc or click on backdrop to close
+    const modalConfigs: NgbModalOptions = {backdrop: 'static', keyboard: false};
+    // dynamically create the modal
+    const modalRef = this.modalService.open(YesNoModalViewComponent, modalConfigs);
+    // cast the modal to the correct type for easier property accessing
+    if(modalRef.componentInstance instanceof YesNoModalViewComponent) {
+      const header: string = 'Warning'
+      let body: string = 'Your current order belongs to a different merchant. ';
+      body += 'Discard all items in current order and start a new order?';
+      modalRef.componentInstance.init(header, body);
+      modalRef.componentInstance.onClose
+        .pipe(take(1))
+        .subscribe( (userClickYes: boolean) => {
+          if(userClickYes) {
+            this.cartService.startNewOrder(container);
+            this.addToOrder(container.product);
+          }
+        });
+    }
   }
 
 }
