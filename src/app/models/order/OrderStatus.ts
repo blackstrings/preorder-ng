92ff6e35b67baa4cab7df29b26808a3ac190cbdf
blackstrings <black_strings0@hotmail.com>
2021-06-07 20:12:58 -0500

    update order status

--- src/app/models/order/OrderStatus.ts
+++ src/app/models/order/OrderStatus.ts
@@ -5,14 +5,35 @@
 export class OrderStatus {
   protected static values: Map<string, OrderStatus> = new Map<string, OrderStatus>();
 
+  // fresh new order, clean, just started, and no items added
   public static readonly NONE = new OrderStatus('none');
-  public static readonly PROCESSING = new OrderStatus('processing');
-  public static readonly PICKUP_READY = new OrderStatus('pickup_ready');
-  public static readonly COMPLETE = new OrderStatus('complete');
-  public static readonly CANCELLED = new OrderStatus('cancelled');
 
-  // test
-  public static readonly CART = new OrderStatus('cart');
+  /** user has moved their order to checkout */
+  public static readonly PENDING_FOR_SUBMISSION = new OrderStatus('pending_for_submission');
+
+  /**
+   * user has placed their order
+   * Order has been submitted to backend with payment, the vendor will soon get notify if success
+   */
+  public static readonly submitted = new OrderStatus('submitted');
+
+  /** order most likely paid for and merchant accepted order and is working on it */
+  public static readonly PROCESSING = new OrderStatus('processing');
+
+  /** vendor has completed the order or is ready for pickup */
+  public static readonly ORDER_READY = new OrderStatus('order_ready');  // order ready for pickup
+
+  /** the order has been delivered or picked up */
+  public static readonly COMPLETED = new OrderStatus('completed'); // order received
+
+  /**
+   * the order was cancelled - to know reason of cancel must look further into the cancellation type
+   * This can be due to many reasons, errors, invalid time frame, overdue timeframe
+   * canceled by vendor/merchant/corporation
+   * Note: A cancelled order cannot and should not be reopened.
+   * If it needs to reopen, create a new order instead based off the cancelled order with valid params.
+   */
+  public static readonly CANCELLED = new OrderStatus('cancelled');
 
   protected constructor(protected value: string) {
     OrderStatus.values.set(value, this);
